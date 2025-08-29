3184a146916bdeb299b71384a07eaf64ec563256
Luiz <luiz7tenshi@gmail.com>
2025-08-29 17:34:41 -0300

    [FEAT]: add Company Document

--- src/models/company_model.py
+++ src/models/company_model.py
@@ -1,46 +1,47 @@
-from sqlalchemy import Column, Integer, String, Boolean, DateTime, func
-from sqlalchemy.orm import relationship
-from database_connection import Base
-from beanie import Document
+# from sqlalchemy import Column, Integer, String, Boolean, DateTime, func
+# from sqlalchemy.orm import relationship
+# from database_connection import Base
+# from beanie import Document
 
-class Company(Base):
-    __tablename__ = 'companies'
+# class Company(Base):
+#     __tablename__ = 'companies'
     
-    id = Column(Integer, primary_key=True, index=True)
-    legal_name = Column(String(255), nullable=False)           # razão social
-    trade_name = Column(String(255))                           # nome fantasia
-    state_registration = Column(String(20))                    # inscrição estadual
-    cnpj = Column(String(14), nullable=False, unique=True)
-    cellphone = Column(String(20))
-    email = Column(String(255), nullable=False, unique=True)
-    address_street = Column(String(255))
-    address_number = Column(String(20))
-    address_info = Column(String(100))
-    address_neighborhood = Column(String(100))
-    address_city = Column(String(100))
-    address_state = Column(String(20))
-    postal_code = Column(String(20))
-    active = Column(Boolean, default=True)
-    signature_date = Column(DateTime)
-    plan = Column(Integer, default=1)
-    trial_start_date = Column(DateTime)
-    trial_end_date = Column(DateTime)
-    trial_used = Column(Boolean, default=False)
-    created_at = Column(DateTime, server_default=func.now()) 
-    updated_at = Column(DateTime, server_default=func.now(), onupdate=func.now())
+#     id = Column(Integer, primary_key=True, index=True)
+#     legal_name = Column(String(255), nullable=False)           # razão social
+#     trade_name = Column(String(255))                           # nome fantasia
+#     state_registration = Column(String(20))                    # inscrição estadual
+#     cnpj = Column(String(14), nullable=False, unique=True)
+#     cellphone = Column(String(20))
+#     email = Column(String(255), nullable=False, unique=True)
+#     address_street = Column(String(255))
+#     address_number = Column(String(20))
+#     address_info = Column(String(100))
+#     address_neighborhood = Column(String(100))
+#     address_city = Column(String(100))
+#     address_state = Column(String(20))
+#     postal_code = Column(String(20))
+#     active = Column(Boolean, default=True)
+#     signature_date = Column(DateTime)
+#     plan = Column(Integer, default=1)
+#     trial_start_date = Column(DateTime)
+#     trial_end_date = Column(DateTime)
+#     trial_used = Column(Boolean, default=False)
+#     created_at = Column(DateTime, server_default=func.now()) 
+#     updated_at = Column(DateTime, server_default=func.now(), onupdate=func.now())
 
-    fiscal_invoices = relationship("FiscalInvoice", back_populates="company")
-    users = relationship("User", back_populates="company")
+#     fiscal_invoices = relationship("FiscalInvoice", back_populates="company")
+#     users = relationship("User", back_populates="company")
 
 
 
-from beanie import Document
-from pydantic import EmailStr
+from beanie import Document, PydanticObjectId
+from pydantic import EmailStr, Field
 from typing import Optional
 from datetime import datetime
 from src.enums.company_enums import CompanyPlanEnum
 
 class Company(Document):
+    id: PydanticObjectId = Field(default_factory=PydanticObjectId, alias="_id")
     legal_name: str
     trade_name: Optional[str]
     state_registration: Optional[str]
@@ -55,13 +56,13 @@ class Company(Document):
     address_state: Optional[str]
     postal_code: Optional[str]
     active: bool = True
-    signature_date: Optional[datetime]
+    signature_date: Optional[datetime] = None
     plan: CompanyPlanEnum = CompanyPlanEnum.free
-    trial_start_date: Optional[datetime]
-    trial_end_date: Optional[datetime]
-    trial_used: bool = False
-    created_at: Optional[datetime] = datetime.utcnow()
-    updated_at: Optional[datetime] = datetime.utcnow()
+    trial_start_date: Optional[datetime] = None
+    trial_end_date: Optional[datetime] = None
+    trial_used: bool = False    
+    created_at: datetime = Field(default_factory=datetime.now)
+    updated_at: datetime = Field(default_factory=datetime.now)
 
     class Settings:
         name = "companies"  # nome da collection no Mongo
--- src/schemas/company_schema.py
+++ src/schemas/company_schema.py
@@ -1,15 +1,16 @@
-from pydantic import BaseModel
+from datetime import datetime
+from beanie import PydanticObjectId
+from pydantic import BaseModel, EmailStr
 from typing import Optional
 from src.enums.company_enums import CompanyPlanEnum
 
-
 class CreateCompany(BaseModel):
     legal_name: str
-    trade_name: str
-    state_registration: str
+    trade_name: Optional[str]
+    state_registration: Optional[str]
     cnpj: str
-    cellphone: str
-    email: str
+    cellphone: Optional[str]
+    email: EmailStr
     address_street: Optional[str]
     address_number: Optional[str]
     address_info: Optional[str]
@@ -18,52 +19,52 @@ class CreateCompany(BaseModel):
     address_state: Optional[str]
     postal_code: Optional[str]
     active: bool = True
-    signature_date: Optional[str]
     plan: CompanyPlanEnum = CompanyPlanEnum.free 
-    trial_start_date: Optional[str]
-    trial_end_date: Optional[str]
     trial_used: bool = False
-    created_at: Optional[str]
-    updated_at: Optional[str]
+    created_at: Optional[datetime] = None
+    updated_at: Optional[datetime] = None
+    # signature_date: Optional[datetime] = None
+    # trial_start_date: Optional[datetime] = None
+    # trial_end_date: Optional[datetime] = None
 
     class Config:
-        orm_mode: True
+        from_attributes = True
 
 class CompanyResponse(BaseModel):
-    id: str
-    legal_name: str
-    trade_name: str
-    state_registration: str
-    cnpj: str
-    cellphone: str
-    email: str
-    address_street: Optional[str]
-    address_number: Optional[str]
-    address_info: Optional[str]
-    address_neighborhood: Optional[str]
-    address_city: Optional[str]
-    address_state: Optional[str]
-    postal_code: Optional[str]
-    active: bool = True
-    signature_date: Optional[str]
-    plan: CompanyPlanEnum = CompanyPlanEnum.free 
-    trial_start_date: Optional[str]
-    trial_end_date: Optional[str]
-    trial_used: bool = False
-    created_at: Optional[str]
-    updated_at: Optional[str]
-
-    class Config:
-        orm_mode: True
-
-class UpdateCompany(BaseModel):
-    id: str
+    id: PydanticObjectId
     legal_name: Optional[str]
     trade_name: Optional[str]
     state_registration: Optional[str]
     cnpj: Optional[str]
     cellphone: Optional[str]
-    email: Optional[str]
+    email: str
+    address_street: Optional[str]
+    address_number: Optional[str]
+    address_info: Optional[str]
+    address_neighborhood: Optional[str]
+    address_city: Optional[str]
+    address_state: Optional[str]
+    postal_code: Optional[str]
+    active: bool
+    plan: CompanyPlanEnum = CompanyPlanEnum.free 
+    signature_date: Optional[str] = None
+    trial_start_date: Optional[str] = None
+    trial_end_date: Optional[str] = None
+    trial_used: bool
+    created_at: Optional[str]
+    updated_at: Optional[str]
+
+    class Config:
+        from_attributes: True
+
+class UpdateCompany(BaseModel):
+    id: PydanticObjectId
+    legal_name: Optional[str]
+    trade_name: Optional[str]
+    state_registration: Optional[str]
+    cnpj: Optional[str]
+    cellphone: Optional[str]
+    email: Optional[EmailStr]
     address_street: Optional[str]
     address_number: Optional[str]
     address_info: Optional[str]
@@ -72,13 +73,12 @@ class UpdateCompany(BaseModel):
     address_state: Optional[str]
     postal_code: Optional[str]
     active: bool = True
-    signature_date: Optional[str]
     plan: CompanyPlanEnum
-    trial_start_date: Optional[str]
-    trial_end_date: Optional[str]
+    signature_date: Optional[datetime] = None
+    trial_start_date: Optional[datetime] = None
+    trial_end_date: Optional[datetime] = None
     trial_used: bool = False
-    created_at: Optional[str]
-    updated_at: Optional[str]
+    updated_at: Optional[datetime] = datetime
 
     class Config:
-        orm_mode: True
+        from_attributes: True
